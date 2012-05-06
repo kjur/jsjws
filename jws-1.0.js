955c0c4f31f383573512930c45de39385692d4f4
Kenji Urushima <kenji.urushima@gmail.com>
2012-05-06 10:00:09 +0900

    1.0.1 release - API document is added

--- jws-1.0.js
+++ jws-1.0.js
@@ -3,7 +3,7 @@
 /*
  * jws.js - JSON Web Signature Class
  *
- * version: 1.0 (04 May 2012)
+ * version: 1.0.1 (06 May 2012)
  *
  * Copyright (c) 2010-2012 Kenji Urushima (kenji.urushima@gmail.com)
  *
@@ -30,6 +30,15 @@ function _jws_verifySignature(sHead, sPayload, hSig, hN, hE) {
     return _rsasign_verifySignatureWithArgs(sSignatureInput, biSig, hN, hE);
 }
 
+/**
+ * get Encoed Signature Value from JWS string.<br/>
+ * @name getEncodedSignatureValueFromJWS
+ * @memberOf JWS
+ * @function
+ * @param {String} sJWS JWS signature string to be verified
+ * @return {String} string of Encoded Signature Value 
+ * @throws if sJWS is not comma separated string such like "Header.Payload.Signature".
+ */
 function _jws_getEncodedSignatureValueFromJWS(sJWS) {
     if (sJWS.match(/^[^.]+\.[^.]+\.([^.]+)$/) == null) {
 	throw "JWS signature is not a form of 'Head.Payload.SigValue'.";
@@ -37,6 +46,19 @@ function _jws_getEncodedSignatureValueFromJWS(sJWS) {
     return RegExp.$1;
 }
 
+/**
+ * verify JWS signature by RSA public key.<br/>
+ * This only supports "RS256" and "RS512" algorithm.
+ * @name verifyJWSByNE
+ * @memberOf JWS
+ * @function
+ * @param {String} sJWS JWS signature string to be verified
+ * @param {String} hN hexadecimal string for modulus of RSA public key
+ * @param {String} hE hexadecimal string for public exponent of RSA public key
+ * @return {String} returns 1 when JWS signature is valid, otherwise returns 0
+ * @throws if sJWS is not comma separated string such like "Header.Payload.Signature".
+ * @throws if JWS Header is a malformed JSON string.
+ */
 function _jws_verifyJWSByNE(sJWS, hN, hE) {
     if (sJWS.match(/^([^.]+)\.([^.]+)\.([^.]+)$/) == null) {
 	throw "JWS signature is not a form of 'Head.Payload.SigValue'.";
@@ -93,6 +115,21 @@ function _jws_generateSignatureValueByNED(sHead, sPayload, hN, hE, hD) {
     return _jws_generateSignatureValueBySI_NED(sHead, sPayload, sSI, hN, hE, hD);
 }
 
+/**
+ * generate JWS signature by Header, Payload and a RSA private key.<br/>
+ * This only supports "RS256" and "RS512" algorithm.
+ * @name generateJWSByNED
+ * @memberOf JWS
+ * @function
+ * @param {String} sHead string of JWS Header
+ * @param {String} sPayload string of JWS Payload
+ * @param {String} hN hexadecimal string for modulus of RSA public key
+ * @param {String} hE hexadecimal string for public exponent of RSA public key
+ * @param {String} hD hexadecimal string for private exponent of RSA private key
+ * @return {String} JWS signature string
+ * @throws if sHead is a malformed JSON string.
+ * @throws if supported signature algorithm was not specified in JSON Header.
+ */
 function _jws_generateJWSByNED(sHead, sPayload, hN, hE, hD) {
     if (! this.isSafeJSONString(sHead)) throw "JWS Head is not safe JSON string: " + sHead;
     var sSI = _getSignatureInputByString(sHead, sPayload);
@@ -101,6 +138,16 @@ function _jws_generateJWSByNED(sHead, sPayload, hN, hE, hD) {
     return sSI + "." + b64SigValue;
 }
 
+/**
+ * check whether a String "s" is a safe JSON string or not.<br/>
+ * If a String "s" is a malformed JSON string or an other object type
+ * this returns 0, otherwise this returns 1.
+ * @name isSafeJSONString
+ * @memberOf JWS
+ * @function
+ * @param {String} s JSON string
+ * @return {Number} 1 or 0
+ */
 function _jws_isSafeJSONString(s) {
   var o = null;
   try {
@@ -113,6 +160,25 @@ function _jws_isSafeJSONString(s) {
   }
 }
 
+/**
+ * JSON Web Signature(JWS) class.<br/>
+ * @property {Dictionary} parsedJWS This property is set after JWS signature verification. <br/>
+ *           Following "parsedJWS_*" properties can be accessed as "parsedJWS.*" because of
+ *           JsDoc restriction.
+ * @property {String} parsedJWS_headB64U string of Encrypted JWS Header
+ * @property {String} parsedJWS_payloadB64U string of Encrypted JWS Payload
+ * @property {String} parsedJWS_sigvalB64U string of Encrypted JWS signature value
+ * @property {String} parsedJWS_si string of Signature Input
+ * @property {String} parsedJWS_sigvalH hexadecimal string of JWS signature value
+ * @property {String} parsedJWS_headS string of decoded JWS Header
+ * @property {String} parsedJWS_headS string of decoded JWS Payload
+ * @class JSON Web Signature(JWS) class
+ * @author Kenji Urushima
+ * @version 1.0.1 (06 May 2012)
+ * @requires base64x.js, json-sans-eval.js and jsrsasign library
+ * @see <a href="http://kjur.github.com/jsjws/">'jwjws'(JWS JavaScript Library) home page http://kjur.github.com/jsjws/</a>
+ * @see <a href="http://kjur.github.com/jsrsasigns/">'jwrsasign'(RSA Sign JavaScript Library) home page http://kjur.github.com/jsrsasign/</a>
+ */
 function JWS() {
 }
 
