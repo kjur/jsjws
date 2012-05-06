e7272b85e77cd3c42bfb5f4c09f4b21ab9cbe38a
Kenji Urushima <kenji.urushima@gmail.com>
2012-05-06 09:52:41 +0900

    1.0.1 release - add API document

--- base64x-1.0.js
+++ base64x-1.0.js
@@ -3,7 +3,7 @@
 /*
  * base64x.js - Base64url and supplementary functions for Tom Wu's base64.js library
  *
- * version: 1.0 (04 May 2012)
+ * version: 1.0.1 (06 May 2012)
  *
  * Copyright (c) 2012 Kenji Urushima (kenji.urushima@gmail.com)
  *
@@ -17,6 +17,37 @@
  *   - base64.js - Tom Wu's Base64 library
  */
 
+/**
+ * Base64URL and supplementary functions for Tom Wu's base64.js library.<br/>
+ * This class is just provide information about global functions
+ * defined in 'base64x.js'. The 'base64x.js' script file provides
+ * global functions for converting following data each other.
+ * <ul>
+ * <li>(ASCII) String</li>
+ * <li>byte array</li>
+ * <li>hexadecimal encoded String</li>
+ * <li>Base64 encoded String</li>
+ * <li>Base64URL encoded String</li>
+ * </ul>
+ * All functions in 'base64x.js' are defined in {@link _global_} and not
+ * in this class.
+ * 
+ * @class Base64URL and supplementary functions for Tom Wu's base64.js library
+ * @author Kenji Urushima
+ * @version 1.0.1 (06 May 2012)
+ * @requires base64.js
+ * @see <a href="http://kjur.github.com/jsjws/">'jwjws'(JWS JavaScript Library) home page http://kjur.github.com/jsjws/</a>
+ * @see <a href="http://kjur.github.com/jsrsasigns/">'jwrsasign'(RSA Sign JavaScript Library) home page http://kjur.github.com/jsrsasign/</a>
+ */
+function Base64x() {
+}
+
+// ==== string / byte array ================================
+/**
+ * convert a string to an array of character codes
+ * @param {String} s
+ * @return {Array of Numbers} 
+ */
 function stoBA(s) {
     var a = new Array();
     for (var i = 0; i < s.length; i++) {
@@ -25,6 +56,11 @@ function stoBA(s) {
     return a;
 }
 
+/**
+ * convert an array of character codes to a string
+ * @param {Array of Numbers} a array of character codes
+ * @return {String} s
+ */
 function BAtos(a) {
     var s = "";
     for (var i = 0; i < a.length; i++) {
@@ -33,6 +69,12 @@ function BAtos(a) {
     return s;
 }
 
+// ==== byte array / hex ================================
+/**
+ * convert an array of bytes(Number) to hexadecimal string.<br/>
+ * @param {Array of Numbers} a array of bytes
+ * @return {String} hexadecimal string
+ */
 function BAtohex(a) {
     var s = "";
     for (var i = 0; i < a.length; i++) {
@@ -43,22 +85,56 @@ function BAtohex(a) {
     return s;
 }
 
+// ==== string / hex ================================
+/**
+ * convert a ASCII string to a hexadecimal string of ASCII codes.<br/>
+ * NOTE: This can't be used for non ASCII characters.
+ * @param {s} s ASCII string
+ * @return {String} hexadecimal string
+ */
 function stohex(s) {
     return BAtohex(stoBA(s));
 }
 
+// ==== string / base64 ================================
+/**
+ * convert a ASCII string to a Base64 encoded string.<br/>
+ * NOTE: This can't be used for non ASCII characters.
+ * @param {s} s ASCII string
+ * @return {String} Base64 encoded string
+ */
 function stob64(s) {
     return hex2b64(stohex(s));
 }
 
+// ==== string / base64url ================================
+/**
+ * convert a ASCII string to a Base64URL encoded string.<br/>
+ * NOTE: This can't be used for non ASCII characters.
+ * @param {s} s ASCII string
+ * @return {String} Base64URL encoded string
+ */
 function stob64u(s) {
     return b64tob64u(hex2b64(stohex(s)));
 }
 
+/**
+ * convert a Base64URL encoded string to a ASCII string.<br/>
+ * NOTE: This can't be used for Base64URL encoded non ASCII characters.
+ * @param {s} s Base64URL encoded string
+ * @return {String} ASCII string
+ */
 function b64utos(s) {
     return BAtos(b64toBA(b64utob64(s)));
 }
 
+// ==== base64 / base64url ================================
+/**
+ * convert a Base64 encoded string to a Base64URL encoded string.<br/>
+ * Example: "ab+c3f/==" &rarr; "ab-c3f_"
+ * @param {String} s Base64 encoded string
+ * @return {String} Base64URL encoded string
+ */
 function b64tob64u(s) {
     s = s.replace(/\=/g, "");
     s = s.replace(/\+/g, "-");
@@ -66,6 +142,12 @@ function b64tob64u(s) {
     return s;
 }
 
+/**
+ * convert a Base64URL encoded string to a Base64 encoded string.<br/>
+ * Example: "ab-c3f_" &rarr; "ab+c3f/=="
+ * @param {String} s Base64URL encoded string
+ * @return {String} Base64 encoded string
+ */
 function b64utob64(s) {
     if (s.length % 3 == 1) s = s + "==";
     if (s.length % 3 == 2) s = s + "=";
@@ -74,19 +156,43 @@ function b64utob64(s) {
     return s;
 }
 
+// ==== hex / base64url ================================
+/**
+ * convert a hexadecimal string to a Base64URL encoded string.<br/>
+ * @param {String} s hexadecimal string
+ * @return {String} Base64URL encoded string
+ */
 function hextob64u(s) {
     return b64tob64u(hex2b64(s));
 }
 
+/**
+ * convert a Base64URL encoded string to a hexadecimal string.<br/>
+ * @param {String} s Base64URL encoded string
+ * @return {String} hexadecimal string
+ */
 function b64utohex(s) {
     return b64tohex(b64utob64(s));
 }
 
+// ==== new lines ================================
+/**
+ * convert all DOS new line("\r\n") to UNIX new line("\n") in 
+ * a String "s".
+ * @param {String} s string 
+ * @return {String} converted string
+ */
 function newline_toUnix(s) {
     s = s.replace(/\r\n/mg, "\n");
     return s;
 }
 
+/**
+ * convert all UNIX new line("\r\n") to DOS new line("\n") in 
+ * a String "s".
+ * @param {String} s string 
+ * @return {String} converted string
+ */
 function newline_toDos(s) {
     s = s.replace(/\r\n/mg, "\n");
     s = s.replace(/\n/mg, "\r\n");
